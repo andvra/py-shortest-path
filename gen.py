7f42913e008f9b9f5c1c892bf28ca1f35a9ca39f
Andreas Vrålstad <andreas.vralstad@gmail.com>
2019-06-27 13:44:29 +0200

    Can  now use CLI mode. Also added a square shape

--- gen.py
+++ gen.py
@@ -5,15 +5,41 @@ import numpy as np
 import operator
 import pygame.freetype
 
-def init_nodes(n, is_circle=False):
-    left = 100
-    top = 100
+def init_nodes(n, fig_shape):
     width = 400
     height = 400
-    if is_circle:
-        return [(int(left+width/2+(width/2)*math.sin(2*math.pi*i/n)), int(top+height/2+(height/2)*math.cos(2*math.pi*i/n))) for i in range(n)]
+    # 1 = circle
+    # 2 = rectangle
+    # 3 = random
+    if fig_shape==1:
+        return [(int(width/2+(width/2)*math.sin(2*math.pi*i/n)), int(height/2+(height/2)*math.cos(2*math.pi*i/n))) for i in range(n)]
+    elif fig_shape==2:
+        node_d = (width+height)*2/n
+        nodes = []
+        for i in range(n):
+            tot_d = i*node_d
+            pos_x = 0
+            if tot_d<width:
+                pos_x=tot_d
+            elif tot_d<width+height:
+                pos_x=width
+            elif tot_d<2*width+height:
+                pos_x=2*width+height-tot_d
+            pos_y = 0
+            if tot_d<width:
+                pos_y=0
+            elif tot_d<width+height:
+                pos_y=tot_d-height
+            elif tot_d<2*width+height:
+                pos_y=height
+            else:
+                pos_y=height-(tot_d-2*width-height)
+            pos_x=int(pos_x)
+            pos_y=int(pos_y)
+            nodes.append((pos_x,pos_y))
+        return nodes
     else:
-        return [(left+random.randint(0,width), top+random.randint(0,height)) for _ in range(n)]
+        return [(random.randint(0,width), random.randint(0,height)) for _ in range(n)]
 
 def init_population(no_nodes, pop_size):
     return [random.sample(range(no_nodes), no_nodes) for _ in range(pop_size)]
@@ -96,9 +122,12 @@ def breed(population):
     return new_population
 
 def draw_nodes(nodes, offset=(0,0)):
-    for node in nodes:
+    col_diff=255//len(nodes)
+    for i, node in enumerate(nodes):
+        col = (255-col_diff*i,128+0.5*col_diff*i,255)
         pos = tuple(map(operator.add, node, offset))
-        pygame.draw.circle(screen, col_node, pos, 5)
+        s = 5
+        pygame.draw.circle(screen, col, pos, s)
 
 def draw_edges(path, offset=(0,0)):
     for es, ee in path:
@@ -106,62 +135,101 @@ def draw_edges(path, offset=(0,0)):
         end = tuple(map(operator.add,ee,offset))
         pygame.draw.line(screen, col_line, start, end, 5)
 
-size = width, height = 1300, 600
-col_bg = (50,50,50)
-col_node = (150,150,150)
-col_line = (100,100,100)
-col_txt = (200,200,50)
-no_nodes = 50
-pop_size = no_nodes*15
-pygame.init()
-font = pygame.freetype.SysFont("Arial", 24)
-screen = pygame.display.set_mode(size)
+def print_usage():
+    print("Usage:")
+    print("   -s=[n] (optional)")
+    print("       1 = circle, 2 = square, 3 = random")
+    print("   -n=[n] (optional)")
+    print("       Number of nodes to use")
+    sys.exit()
 
-nodes = init_nodes(no_nodes, True)
-population = init_population(no_nodes, pop_size)
-population, best_paths, best_ds = order_by_fitness(nodes, population)
-best_path = best_paths[0]
-best_d = best_ds[0]
-alltime_best_path = best_path
-alltime_best_d = best_d
-alltime_best_gen = 1
-txt_best_d, _ = font.render("Shortest: {:.1f}".format(alltime_best_d),col_txt)
-txt_best_gen, _ = font.render("Generation: {}".format(alltime_best_gen),col_txt)
-txt_fps, _ = font.render("FPS")
+def parse_arguments(argv):
+    fig_shape = 1
+    no_nodes = 50
+    use_gfx = True
+    for arg in argv[1:]:
+        if len(arg)>0 and arg[0]=='-':
+            if arg.count("=")==0:
+                k = arg[1:]
+            else:
+                k, v = arg[1:].split('=')
+            if k=="s":
+                fig_shape=int(v)
+            elif k=="n":
+                no_nodes = int(v)
+            elif k=="c":
+                use_gfx = False
+            else:
+                print_usage()
+        else:
+            print_usage()
+    return no_nodes, fig_shape, use_gfx
 
-running = True
-cur_gen = 1
-clk = pygame.time.Clock()
-last_update = 0
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key==pygame.K_ESCAPE:
-            running = False
-    clk.tick()
-    if last_update+1000<pygame.time.get_ticks():
-        fps = clk.get_fps()
-        txt_fps, _ = font.render("FPS: {:.1f}".format(fps), col_txt)
-    screen.fill(col_bg)
-    screen.blit(txt_fps, (1100,50))
-    screen.blit(txt_best_d, (1100,100))
-    screen.blit(txt_best_gen, (1100,150))
-    draw_edges(best_path)
-    draw_nodes(nodes)
-    draw_edges(alltime_best_path, (500,0))
-    draw_nodes(nodes,(500,0))
-    population = breed(population)
+if __name__ == "__main__":
+    no_nodes, fig_shape, use_gfx = parse_arguments(sys.argv)
+    if use_gfx==False:
+        print("Using CLI")
+    pop_size = no_nodes*15
+    nodes = init_nodes(no_nodes, fig_shape)
+    population = init_population(no_nodes, pop_size)
     population, best_paths, best_ds = order_by_fitness(nodes, population)
     best_path = best_paths[0]
     best_d = best_ds[0]
-    cur_gen += 1
-    if best_d<alltime_best_d:
-        alltime_best_d=best_d
-        alltime_best_path=best_path
-        alltime_best_gen = cur_gen
+    alltime_best_path = best_path
+    alltime_best_d = best_d
+    alltime_best_gen = 1
+    pygame.init()
+    clk = pygame.time.Clock()
+    if use_gfx:
+        size = width, height = 1300, 600
+        col_bg = (50,50,50)
+        col_node = (150,150,150)
+        col_line = (100,100,100)
+        col_txt = (200,200,50)
+        font = pygame.freetype.SysFont("Arial", 24)
+        screen = pygame.display.set_mode(size)
         txt_best_d, _ = font.render("Shortest: {:.1f}".format(alltime_best_d),col_txt)
         txt_best_gen, _ = font.render("Generation: {}".format(alltime_best_gen),col_txt)
-    pygame.display.flip()
+        txt_fps, _ = font.render("FPS")
 
-pygame.quit()
+    running = True
+    cur_gen = 1
+    last_update = 0
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key==pygame.K_ESCAPE:
+                running = False
+        if use_gfx:
+            screen.fill(col_bg)
+            screen.blit(txt_fps, (1100,50))
+            screen.blit(txt_best_d, (1100,100))
+            screen.blit(txt_best_gen, (1100,150))
+            draw_edges(best_path,(100,100))
+            draw_nodes(nodes,(100,100))
+            draw_edges(alltime_best_path, (600,100))
+            draw_nodes(nodes,(600,100))
+        clk.tick()
+        if last_update+1000<pygame.time.get_ticks():
+            fps = clk.get_fps()
+            if use_gfx:
+                txt_fps, _ = font.render("FPS: {:.1f}".format(fps), col_txt)
+            else:
+                print("Shortest: {:.1f} Generation: {} FPS: {:.1f}".format(alltime_best_d,alltime_best_gen,fps),end="\r")
+        population = breed(population)
+        population, best_paths, best_ds = order_by_fitness(nodes, population)
+        best_path = best_paths[0]
+        best_d = best_ds[0]
+        cur_gen += 1
+        if best_d<alltime_best_d:
+            alltime_best_d=best_d
+            alltime_best_path=best_path
+            alltime_best_gen = cur_gen
+            if use_gfx:
+                txt_best_d, _ = font.render("Shortest: {:.1f}".format(alltime_best_d),col_txt)
+                txt_best_gen, _ = font.render("Generation: {}".format(alltime_best_gen),col_txt)
+        if use_gfx:
+            pygame.display.flip()
+
+    pygame.quit()
